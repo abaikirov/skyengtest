a9da2610a37e0d9d6ee6baed2004eaed13b37be0
Abai Abakirov <abaikirov@gmail.com>
2020-06-12 17:06:02 +0600

    Initial Commit

---
+++ SkyEngTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyEngTest
+//
+//  Created by Abai Abakirov on 6/12/20.
+//  Copyright © 2020 Abaikirov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
